a0e468932c32c66976babfe1077ae4514e65ae5d
okancandev <okancan.dev@gmail.com>
2025-01-22 00:37:46 +0300

    make scope calls always call main one
    even if previous version is faster, this way much parallel as hierarchical scope and easier to debug for end user

--- Assets/Scripts/Scopy/Runtime/Scope.cs
+++ Assets/Scripts/Scopy/Runtime/Scope.cs
@@ -17,47 +17,47 @@ namespace Okancandev.Scopy
 
         public void AddSingle(object service)
         {
-            _services.Add(new ServiceIdentifier(service.GetType()), service);
+            Add(new ServiceIdentifier(service.GetType()), service);
         }
         
         public void AddWithId(object service, long id)
         {
-            _services.Add(new ServiceIdentifier(service.GetType(), id), service);
+            Add(new ServiceIdentifier(service.GetType(), id), service);
         }
         
         public void AddTagged(object service, object tag)
         {
-            _services.Add(new ServiceIdentifier(service.GetType(), tag), service);
+            Add(new ServiceIdentifier(service.GetType(), tag), service);
         }
 
         public void AddTaggedWithId(object service, long id, object tag)
         {
-            _services.Add(new ServiceIdentifier(service.GetType(), id, tag), service);
+            Add(new ServiceIdentifier(service.GetType(), id, tag), service);
+        }
+        
+        public bool Remove(ServiceIdentifier identifier) 
+        {
+            return _services.Remove(identifier);
         }
         
         public void RemoveSingle(object service)
         {
-            _services.Remove(new ServiceIdentifier(service.GetType()));
+            Remove(new ServiceIdentifier(service.GetType()));
         }
         
         public void RemoveWithId(object service, long id)
         {
-            _services.Remove(new ServiceIdentifier(service.GetType(), id));
+            Remove(new ServiceIdentifier(service.GetType(), id));
         }
         
         public void RemoveTagged(object service, object tag)
         {
-            _services.Remove(new ServiceIdentifier(service.GetType(), tag));
+            Remove(new ServiceIdentifier(service.GetType(), tag));
         }
 
         public void RemoveTaggedWithId(object service, long id, object tag)
         {
-            _services.Remove(new ServiceIdentifier(service.GetType(), id, tag));
-        }
-    
-        public bool Remove(ServiceIdentifier identifier) 
-        {
-            return _services.Remove(identifier);
+            Remove(new ServiceIdentifier(service.GetType(), id, tag));
         }
         
         public object Get(ServiceIdentifier identifier) 
@@ -79,88 +79,88 @@ namespace Okancandev.Scopy
         
         public object GetSingle(Type type)
         {
-            return _services[new ServiceIdentifier(type)];
+            return Get(new ServiceIdentifier(type));
         }
         
         public object GetWithId(Type type ,long id)
         {
-            return _services[new ServiceIdentifier(type, id)];
+            return Get(new ServiceIdentifier(type, id));
         }
         
         public object GetTagged(Type type, object tag)
         {
-            return _services[new ServiceIdentifier(type, tag)];
+            return Get(new ServiceIdentifier(type, tag));
         }
         
         public object GetTaggedWithId(Type type, long id, object tag)
         {
-            return _services[new ServiceIdentifier(type, id, tag)];
+            return Get(new ServiceIdentifier(type, id, tag));
         }
         
         public object GetSingle<T>()
         {
-            return _services[new ServiceIdentifier(typeof(T))];
+            return Get(new ServiceIdentifier(typeof(T)));
         }
         
         public T GetWithId<T>(long id)
         {
-            return (T)_services[new ServiceIdentifier(typeof(T), id)];
+            return (T)Get(new ServiceIdentifier(typeof(T), id));
         }
         
         public T GetTagged<T>(object tag)
         {
-            return (T)_services[new ServiceIdentifier(typeof(T), tag)];
+            return (T)Get(new ServiceIdentifier(typeof(T), tag));
         }
         
         public T GetTaggedWithId<T>(long id, object tag)
         {
-            return (T)_services[new ServiceIdentifier(typeof(T), id, tag)];
+            return (T)Get(new ServiceIdentifier(typeof(T), id, tag));
         }
         
         public bool TryGetSingle(Type serviceType, out object service) 
         {
-            return _services.TryGetValue(new ServiceIdentifier(serviceType), out service);
+            return TryGet(new ServiceIdentifier(serviceType), out service);
         }
         
         public bool TryGetWithId(Type serviceType, long id, out object service) 
         {
-            return _services.TryGetValue(new ServiceIdentifier(serviceType, id), out service);
+            return TryGet(new ServiceIdentifier(serviceType, id), out service);
         }
         
         public bool TryGetTagged(Type serviceType, object tag, out object service) 
         {
-            return _services.TryGetValue(new ServiceIdentifier(serviceType, tag), out service);
+            return TryGet(new ServiceIdentifier(serviceType, tag), out service);
         }
         
         public bool TryGetTaggedWithId(Type serviceType, long id, object tag, out object service) 
         {
-            return _services.TryGetValue(new ServiceIdentifier(serviceType, id, tag), out service);
+            return TryGet(new ServiceIdentifier(serviceType, id, tag), out service);
         }
 
         public bool TryGetSingle<T>(out T service)
         {
-            bool result = _services.TryGetValue(new ServiceIdentifier(typeof(T)), out var value);
+            bool result = TryGet(new ServiceIdentifier(typeof(T)), out var value);
             service = (T)value;
             return result;
         }
         
         public bool TryGetWithId<T>(long id, out object service) 
         {
-            bool result = _services.TryGetValue(new ServiceIdentifier(typeof(T), id), out var value);
+            bool result = TryGet(new ServiceIdentifier(typeof(T), id), out var value);
             service = (T)value;
             return result;
         }
         
         public bool TryGetTagged<T>(object tag, out object service) 
         {
-            bool result = _services.TryGetValue(new ServiceIdentifier(typeof(T), tag), out var value);
+            bool result = TryGet(new ServiceIdentifier(typeof(T), tag), out var value);
             service = (T)value;
             return result;
         }
         
         public bool TryGetTaggedWithId<T>(long id, object tag, out object service) 
         {
-            bool result = _services.TryGetValue(new ServiceIdentifier(typeof(T), id, tag), out var value);
+            bool result = TryGet(new ServiceIdentifier(typeof(T), id, tag), out var value);
             service = (T)value;
             return result;
         }
